fb422a99220b76b3d67da5e2ad7f736b4fff4a3e
quanggg12 <94882579+quanggg12@users.noreply.github.com>
2024-11-30 12:18:35 +0700

    Update nguoidulich.cpp

--- cp/quaylui_nhanhcan/nguoidulich.cpp
+++ cp/quaylui_nhanhcan/nguoidulich.cpp
@@ -1,14 +1,41 @@
 #include <bits/stdc++.h>
-#define ll long long
-
 using namespace std;
 
-const int MOD = 1000000007;
-const int MX = 100001;
+using ll = long long;
 
+int c[100][100];
+int n, X[100]; // X[i] = j : thành phố thăm ở lượt thứ i là thành phố j
+int visited[100], res = INT_MAX, sum = 0;
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    
-    return 0;
+void nhap(){
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++) cin >> c[i][j];
+    }
+    X[1] = 1;
+    visited[1] = 1;
+}
+
+void Try(int i){
+    for(int j = 1; j <= n; j++){
+        if(!visited[j]){
+            visited[j] = 1;
+            X[i] = j;
+            sum += c[X[i - 1]][X[i]];
+            if(i == n){
+                res = min(res, sum + c[X[i]][1]);
+            }
+            else{
+                Try(i + 1);
+            }
+            sum -= c[X[i - 1]][X[i]];
+            visited[j] = 0;
+        }
+    }
+}
+
+int main(){
+    nhap();
+    Try(2);
+    cout << res << endl;
 }
