881d426df93c818ef6a705c63c09c318c4dba7c9
quanggg12 <94882579+quanggg12@users.noreply.github.com>
2024-11-30 20:04:54 +0700

    Update cathinhvuong.cpp

--- dsa-2-main/DP/cathinhvuong.cpp
+++ dsa-2-main/DP/cathinhvuong.cpp
@@ -38,3 +38,38 @@ int main(){
    cout << "so hv it nhat: " << c[n][m];
    return 0;
 }
+
+// int dp[501][501]; 
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int m, n;
+//     cin >> m >> n;
+
+//     // Khởi tạo bảng dp
+//     for (int i = 1; i <= m; i++) {
+//         for (int j = 1; j <= n; j++) {
+//             if (i == j) {
+//                 dp[i][j] = 1; 
+//             } else {
+//                 dp[i][j] = INT_MAX; 
+
+//                 // Cat ngang
+//                 for (int k = 1; k < i; k++) {
+//                     dp[i][j] = min(dp[i][j], dp[k][j] + dp[i - k][j]);
+//                 }
+
+//                 // Cat dọc
+//                 for (int k = 1; k < j; k++) {
+//                     dp[i][j] = min(dp[i][j], dp[i][k] + dp[i][j - k]);
+//                 }
+//             }
+//         }
+//     }
+
+//     cout << dp[m][n];
+
+//     return 0;
+// }
