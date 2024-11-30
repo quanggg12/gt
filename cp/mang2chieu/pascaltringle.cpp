8447b3ee6b806765a355917e8ceec8f3fb403148
quanggg12 <94882579+quanggg12@users.noreply.github.com>
2024-11-30 11:32:06 +0700

    pascaltringle.cpp

--- cp/mang2chieu/pascaltringle.cpp
+++ cp/mang2chieu/pascaltringle.cpp
@@ -1,31 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+
 using namespace std;
 
 
 void printPascal(int n) {
-    int pascalTriangle[n][n];
+    int pascal[n][n];
 
     for (int line = 0; line < n; line++) {
         for (int i = 0; i <= line; i++) {
            
             if (i == 0 || i == line)
-                pascalTriangle[line][i] = 1;
+                pascal[line][i] = 1;
             else
                 
-                pascalTriangle[line][i] = pascalTriangle[line - 1][i - 1] + pascalTriangle[line - 1][i];
+                pascal[line][i] = pascal[line - 1][i - 1] + pascal[line - 1][i];
         }
     }
 
     for (int line = 0; line < n; line++) {
         for (int i = 0; i <= line; i++)
-            cout << pascalTriangle[line][i] << " ";
+            cout << pascal[line][i] << " ";
         cout << endl;
     }
 }
 
 int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
     int n;
-
     cin >> n;
 
     printPascal(n);
