8a950908d9f346ee562a28fed859aba0410255c0
quanggg12 <94882579+quanggg12@users.noreply.github.com>
2024-11-30 11:28:26 +0700

    Update ditu.cpp

--- cp/number_theory/ditu.cpp
+++ cp/number_theory/ditu.cpp
@@ -9,10 +9,48 @@ const int MOD = 1'000'000'007;
 // 5 27 1 2000
 // Output:
 // 1 2 2000
+
+int days_in_month(int month, int year) {
+    if (month == 2) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return 29;
+        } else {
+            return 28;
+        }
+    }
+    if (month == 4 || month == 6 || month == 9 || month == 11) {
+        return 30;
+    }
+    return 31;
+}
+
+void add_days(ll &d, ll &m, ll &y, ll days) {
+    while (days > 0) {
+        int days_in_current_month = days_in_month(m, y);
+        if (d + days <= days_in_current_month) {
+            d += days;
+            days = 0;
+        } else {
+            days -= (days_in_current_month - d + 1);
+            d = 1;
+            if (m == 12) {
+                m = 1;
+                y++;
+            } else {
+                m++;
+            }
+        }
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
     ll x, d, m, y;
     cin >> x >> d >> m >> y;
-        
+
+    add_days(d, m, y, x);
+
+    cout << d << " " << m << " " << y << "\n";
+
     return 0;
 }
