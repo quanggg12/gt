40d62a2b6d7fb67dd9ab674ea0fc66faeb66a183
quanggg12 <94882579+quanggg12@users.noreply.github.com>
2024-12-01 12:15:30 +0700

    Update pascal.cpp

--- cp/mang2chieu/pascal.cpp
+++ cp/mang2chieu/pascal.cpp
@@ -3,27 +3,23 @@
 
 using namespace std;
 
-ll C[1005][1005];
-
-
-void init() {
-    for (int i = 0; i <= 1000; i++) {
-        for (int j = 0; j <= i; j++) {
-            if (j == 0 || j == i) C[i][j] = 1;
-            else C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % 1000000007;
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<ll>> pascal(n);
+ 
+    for (int i = 0; i < n; i++) {
+        pascal[i].resize(i + 1); 
+        pascal[i][0] = pascal[i][i] = 1; 
+        for (int j = 1; j < i; j++) {
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
         }
     }
-}
-
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    init();
-    int n; cin >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) {
-            cout << C[i][j] << " ";
+            cout << pascal[i][j] << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
     return 0;
 }
