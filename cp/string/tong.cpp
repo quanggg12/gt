7dd57f3d35595d092ea342da0040b4088ba3b181
quanggg12 <94882579+quanggg12@users.noreply.github.com>
2024-12-05 10:01:31 +0700

    Update tong.cpp

--- cp/string/tong.cpp
+++ cp/string/tong.cpp
@@ -2,13 +2,11 @@
 #define ll long long
 
 using namespace std;
-const int MAX = 200'007;
-const int MOD = 1'000'000'007;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
     string s; getline(cin, s);
-    int res = 0;
+    ll res = 0;
     stringstream ss(s);
     string token, tmp = "";
     while (ss >> token) {
@@ -18,7 +16,7 @@ int main() {
             }
         }
         if (tmp != "") {
-            res += stoi(tmp);
+            res += stoll(tmp);
             tmp = "";
         }
     }
